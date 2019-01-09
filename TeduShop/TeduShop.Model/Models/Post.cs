e9837e13d9121057ebdabb98197d679452aa1868
Nghia Nguyen <lightheaven98@gmail.com>
2019-01-09 22:10:27 +0700

    add domain entities for project

---
+++ TeduShop/TeduShop.Model/Models/Post.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Posts")]
+    public class Post : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Alias { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public string Image { set; get; }
+
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { set; get; }
+
+        [ForeignKey("CategoryID")]
+        public virtual PostCategory PostCategory { get; set; }
+    }
+}
