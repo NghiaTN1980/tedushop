15a39878294fd9ca3845660c1d1f1eebf6177863
Nghia Nguyen <lightheaven98@gmail.com>
2019-01-12 13:32:09 +0700

    add ASP.NET Identity

--- TeduShop/TeduShop.Data/TeduShopDbContext.cs
+++ TeduShop/TeduShop.Data/TeduShopDbContext.cs
@@ -1,9 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using TeduShop.Model.Models;
 
 namespace TeduShop.Data
 {
-    public class TeduShopDbContext : DbContext
+    public class TeduShopDbContext : IdentityDbContext<ApplicationUser>
     {
         public TeduShopDbContext() : base("TeduShopConnection")
         {
@@ -29,10 +30,17 @@ namespace TeduShop.Data
         public DbSet<VisitorStatistic> VisitorStatistics { set; get; }
         public DbSet<Error> Errors { set; get; }
 
+        public static TeduShopDbContext Create()
+        {
+            return new TeduShopDbContext();
+        }
+
         //Phương thức ghi đè
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId });
+            modelBuilder.Entity<IdentityUserLogin>().HasKey(i => i.UserId);
         }
     }
 }
