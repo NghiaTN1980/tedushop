245888083bf5ef0c777e7aa086c4f2164abf512e
Nghia Nguyen <lightheaven98@gmail.com>
2019-01-13 16:08:21 +0700

    service in auglarjs

--- TeduShop/TeduShop.Web/Scripts/spa/app.js
+++ TeduShop/TeduShop.Web/Scripts/spa/app.js
@@ -2,21 +2,32 @@
 var myApp = angular.module("myModule", []);
 
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service('Validator', Validator);
+
+schoolController.$inject = ['$scope','Validator']
 
 //studentController.$inject = ['$scope'];
 //teacherController.$inject = ['$scope'];
 
 //Declare
-function schoolController($scope){
-    $scope.message = "Announcement from school.";
+function schoolController($scope, Validator)
+{
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    };
+    $scope.num = 1;
 }
 
-function studentController($scope) {
-    $scope.message = "This is my message from Student";
-}
-
-function teacherController($scope) {
-    $scope.message = "This is my message from Teacher";
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else {
+            return 'This is odd';
+        }
+    }
 }
