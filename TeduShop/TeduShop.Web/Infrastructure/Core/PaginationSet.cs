fa83f831f1e726a290c3f308399267a6b2ebf185
Nghia Nguyen <lightheaven98@gmail.com>
2019-01-16 14:59:23 +0700

    add paging for product category

---
+++ TeduShop/TeduShop.Web/Infrastructure/Core/PaginationSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TeduShop.Web.Infrastructure.Core
+{
+    public class PaginationSet<T>
+    {
+        public int Page { set; get; }
+
+        public int Count
+        {
+            get
+            {
+                return (Items != null) ? Items.Count() : 0;
+            }
+        }
+
+        //Tổng số trang
+        public int TotalPages { get; set; }
+        //Tổng số dòng
+        public int TotalCount { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
